ad1c2d60c5fe909157aa052ee00561e0d3d2ccb7
jaykaron <jaykaron@gmail.com>
2018-05-16 12:59:54 -0400

    another spelling mistake ABD instead of ADB

--- test/test_adb.py
+++ test/test_adb.py
@@ -4,7 +4,7 @@ def test_adbt():
     data = '$PADBT,000.000,f,019.04,M,000.000,F*3C'
     msg = pynmea2.parse(data)
     assert type(msg) == pynmea2.adb.ADBT
-    assert msg.manufacturer == 'ABD'
+    assert msg.manufacturer == 'ADB'
     assert msg.subtype == 'T'
     assert msg.dpf == 0
     assert msg.dpf_unit == 'f'
